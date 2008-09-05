4b1b4da92df567b28a63601094e8ad110d1fc682
digitallyborn <digitallyborn@c2970b14-522b-426f-88d6-479d74134ca4>
2008-09-05 16:20:08 +0000

    Addition of GNU License header.
    Issue #5

--- Twiterizer.Framework/TwitterRequest.cs
+++ Twiterizer.Framework/TwitterRequest.cs
@@ -1,3 +1,23 @@
+/*
+* TwitterRequest.cs
+*
+* Copyright © 2008 Patrick "Ricky" Smith <ricky@digitally-born.com>
+*
+* This file is part of the Twitterizer library
+*
+* The Twitterizer library is free software: you can redistribute it
+* and/or modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 using System;
 using System.Text;
 using System.Text.RegularExpressions;
