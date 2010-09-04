9ed7894808be132e55ae877564ea40cb14fe4457
digitallyborn <digitallyborn@c2970b14-522b-426f-88d6-479d74134ca4>
2010-09-04 02:44:55 +0000

    Updated release package script.

--- PackageReleases.bat
+++ PackageReleases.bat
@@ -1,10 +1,30 @@
 @Echo off
 
+REM Ask for the version number of the release
 set twitterizerver=
 set /P twitterizerver=What is the version number? %=%
 
+REM Loop through each directory (one for each package) and zip them up
 FOR /f %%a IN ('dir /b /ad .\Release\') DO (
-PUSHD "Release\%%a\" 
+IF EXIST "%%a-%twitterizerver%.zip" del /Q "%%a-%twitterizerver%.zip"
+PUSHD "Release\%%a" 
 "%ProgramFiles%\7-Zip\7z.exe" a -r "..\%%a-%twitterizerver%.zip" *.*
 POPD
 )
+
+REM Check if the source directory is there from a previous failed attempt and delete it
+IF EXIST "Release\Twitterizer%twitterizerver%-source" RD /S /Q "Release\Twitterizer%twitterizerver%-source"
+
+REM Make our source directory
+mkdir "Release\Twitterizer%twitterizerver%-source"
+
+REM Export our working copy to the source directory
+svn export --force . "Release\Twitterizer%twitterizerver%-source"
+
+REM Zip up the source code
+PUSHD "Release\Twitterizer%twitterizerver%-source"
+"%ProgramFiles%\7-Zip\7z.exe" a -r "..\Twitterizer%twitterizerver%-source.zip" *.*
+popd
+
+REM Cleanup
+RD /S /Q "Release\Twitterizer%twitterizerver%-source"
