d68f77896205e9c3108da32165509714d9141c53
digitallyborn <digitallyborn@c2970b14-522b-426f-88d6-479d74134ca4>
2010-10-30 17:48:04 +0000

    Issue 59

---
+++ Twitterizer2.TestCases/Issue59.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Twitterizer;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Twitterizer2.TestCases
+{
+    [TestFixture]    
+    public class Issue59
+    {
+        [Test]
+        public static void RunTest()
+        {
+            OAuthTokens tokens = Configuration.GetTokens();
+
+            TwitterResponse<TwitterUser> firstUserResponse = TwitterUser.Show(tokens, "twit_er_izer");
+            Assert.IsNotNull(firstUserResponse);
+            Assert.That(firstUserResponse.Result == RequestResult.Success);
+
+            TwitterResponse<TwitterUser> secondUserResponse = TwitterUser.Show(tokens, "twitterapi");
+            Assert.IsNotNull(secondUserResponse);
+            Assert.That(secondUserResponse.Result == RequestResult.Success);
+
+            // This is the query, modified as little as possible.
+            var query = from list in TwitterList.GetLists(tokens, firstUserResponse.ResponseObject.ScreenName).ResponseObject
+                        select new XElement("input", new XAttribute("type", "checkbox"),
+                            TwitterList.CheckMembership(
+                                tokens,
+                                firstUserResponse.ResponseObject.ScreenName,
+                                list.Id.ToString(),
+                                secondUserResponse.ResponseObject.Id).ResponseObject == null ? null : new XAttribute("checked", "")
+                        );
+        }
+    }
+}
