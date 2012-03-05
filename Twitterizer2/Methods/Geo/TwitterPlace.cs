08b8dd4631fc1c8e2b8ec3ae41c37550423caef6
Ricky Smith <ricky@digitally-born.com>
2012-03-05 11:16:02 -0500

    Added a few missing properties to the geo place object.

--- Twitterizer2/Methods/Geo/TwitterPlace.cs
+++ Twitterizer2/Methods/Geo/TwitterPlace.cs
@@ -84,6 +84,46 @@ namespace Twitterizer
         public string StreetAddress { get; set; }
 
         /// <summary>
+        /// Gets or sets the postal code.
+        /// </summary>
+        /// <value>The postal code.</value>
+        /// <remarks></remarks>
+        [JsonProperty(PropertyName = "postal_code")]
+        public string PostalCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number in the preferred local format for the place, include long distance code.
+        /// </summary>
+        /// <value>The phone number.</value>
+        /// <remarks></remarks>
+        [JsonProperty(PropertyName = "phone")]
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the locality.
+        /// </summary>
+        /// <value>The locality.</value>
+        /// <remarks></remarks>
+        [JsonProperty(PropertyName = "locality")]
+        public string Locality { get; set; }
+
+        /// <summary>
+        /// Gets or sets the region.
+        /// </summary>
+        /// <value>The region.</value>
+        /// <remarks></remarks>
+        [JsonProperty(PropertyName = "region")]
+        public string Region { get; set; }
+
+        /// <summary>
+        /// Gets or sets the iso3 country code.
+        /// </summary>
+        /// <value>The iso3 country code.</value>
+        /// <remarks></remarks>
+        [JsonProperty(PropertyName = "iso3")]
+        public string Iso3CountryCode { get; set; }
+
+        /// <summary>
         /// Gets or sets the full name.
         /// </summary>
         /// <value>The full name.</value>
@@ -103,6 +143,13 @@ namespace Twitterizer
         /// <value>The place id.</value>
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets an ID or comma separated list of IDs representing the place in the applications place database.
+        /// </summary>
+        /// <value>The app ids.</value>
+        [JsonProperty(PropertyName = "app:id")]
+        public string AppIds { get; set; }
 
         /// <summary>
         /// Gets or sets the bounding box.
